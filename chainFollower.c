5dd007ae1c0291c8cc42db866b9de4c74da3a27f
Samuel Stephens <mistersamstephens@gmail.com>
2023-05-27 17:41:39 +0100

    remove comments

--- chainFollower.c
+++ chainFollower.c
@@ -16,20 +16,13 @@ int main(int argc, char *argv[]) {
 	return 1;
     }
     while (S_ISLNK(buf.st_mode)){
-	//puts("1");
-	//puts("It's a soft link.");
-    	//printf("The inode number is %lu.\n", buf.st_ino);
-	//printf("attempting to read the link %s, putting result into buffer that currently contains %s, with size %d\n", str, out, 64);
 	int value = readlink(out, out, 64);
 	out[value] = '\0';
-	//printf("going around again, out is %s", out);
 	int r = lstat(out, &buf);
 	if (r != 0){
 	    puts("error baby :(\n");
 	    return 1;
             }
-	//printf("\n\n\nbuf inode is %ld\n\n\n", buf.st_ino);
-	//printf("\nbuf mode is %u\n\n\n", buf.st_mode);
 	printf("%s\n", out);
 	}
     printf("\nfinal destination is: %s\n\n", out);
